48df5351c82e8d558c6c2ee8b8b7c7fc1752bb87
nsubugak <nkasozi@gmail.com>
2018-05-02 20:46:48 +0300

    Added RecievePegPayWebResponse.php
    Php code to recieve and handle response from PegPayWeb

---
+++ RecievePegPayWebResponse.php
@@ -0,0 +1,39 @@
+<?php
+
+ $Status = isset($_GET["Status"])?$_GET["Status"]:"";
+ $Reason = isset($_GET["Reason"])?$_GET["Reason"]:"";
+ $VendorTranId = isset($_GET["VendorID"])?$_GET["VendorID"]:"";
+ $DigitalSignature = isset($_GET["DigitalSignature"])?$_GET["DigitalSignature"]:"";
+ $SecretKey = "T3rr16132016";
+ $DataToVerify= $Status.$Reason.$VendorTranId;
+ $HashAlgorithm = 'sha256';
+ 
+ //verify authenticity of response by confirming 
+ //it has not been tampered with (verify digitalSignature)
+ $ComputedDigitalSignature = hash_hmac($HashAlgorithm,$DataToVerify,$SecretKey);
+ 
+  
+ //something is wrong with this response..its not authentic
+ if($ComputedDigitalSignature!=$DigitalSignature)
+ {
+	 $result = '<div class="alert alert-danger">INVALID RESPONSE. PLEASE CONTACT System ADMINISTRATORS</div>';
+	 echo $result;
+	 return;
+ }
+ 
+ $TransactionId = isset($_GET["TranRef"])?$_GET["TranRef"]:"";
+ 
+ //response is authentic and 
+ //transaction failed
+ if ($Status != "SUCCESS") 
+ {
+	$result = '<div class="alert alert-danger">Transaction Failed :'.$Reason.'</div>';
+	echo $result;
+	return;
+ }
+ 
+ //transaction succeeded
+ $result = '<div class="alert alert-success">Transaction Completed. Receipt Number:'.$TransactionId.'</div>';
+ echo $result;
+ return;
+?> 
